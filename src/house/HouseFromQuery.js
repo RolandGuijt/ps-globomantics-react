7b6bfc12dfb935c48db03867999d6ce76da0b88b
Roland Guijt <roland.guijt@gmail.com>
2021-06-10 15:50:56 +0200

    Fixes

--- src/house/HouseFromQuery.js
+++ src/house/HouseFromQuery.js
@@ -1,12 +1,12 @@
 import { useParams } from "react-router-dom";
-import BaseHouse from "./BaseHouse";
+import House from "./";
 
 const HouseFromQuery = ({ allHouses }) => {
   const { id } = useParams();
   const house = allHouses.find((h) => h.id === parseInt(id));
 
   if (!house) return <div>House not found.</div>;
-  return <BaseHouse house={house}></BaseHouse>;
+  return <House house={house}></House>;
 };
 
 export default HouseFromQuery;
--- src/house/index.js
+++ src/house/index.js
@@ -1,8 +1,39 @@
 import "./house.css";
-import BaseHouse from "./BaseHouse";
+import { useState } from "react";
+import emailIcon from "./Email.png";
+import Inquiry from "./Inquiry";
 
 const House = ({ house }) => {
-  return <BaseHouse house={house}></BaseHouse>;
+  const [inquiryShown, setInquiryShown] = useState(false);
+  const inquiryClick = () => {
+    setInquiryShown(!inquiryShown);
+  };
+  return (
+    <div>
+      <div className="row mt-2">
+        <h5 className="col-md-12">{house.country}</h5>
+      </div>
+      <div className="row">
+        <h3 className="col-md-12">{house.address}</h3>
+      </div>
+      <div className="row">
+        <div className="col-md-7">
+          <img src={`/images/${house.photo}.jpeg`} alt="House" />
+        </div>
+        <div className="col-md-5">
+          <p className="price">${house.price}</p>
+          <p>{house.description}</p>
+          <img
+            src={emailIcon}
+            height="50"
+            alt="inquiry"
+            onClick={inquiryClick}
+          />
+          {inquiryShown && <Inquiry house={house} />}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default House;
